b1b094f4a3502164bd32a2ae5fa82a475a246086
Valliappan <plvalliappan2003@gmail.com>
2024-02-29 17:57:09 +0530

    second

--- dictionary_sequential.c
+++ dictionary_sequential.c
@@ -5,11 +5,10 @@
 #include<unistd.h>
 #include<pthread.h>
 #include<ctype.h>
-int cnt=0, missed=0;
-pthread_rwlock_t countlock;
+
 // constants
 int pages=26, pagelen=200000, datasetlength=50000;
-
+int cnt=0;
 // structure for the page
 typedef struct node{
     void** words;
@@ -17,12 +16,10 @@ typedef struct node{
     int wordcount;
 }page;
 
-page* dict;
-
 // lock for a particular page
 pthread_rwlock_t *pagelock;
 // lock for the entire datastructute
-
+pthread_rwlock_t **lock;
 
 // Creates a random string of size 6
 char *generateword(){
@@ -41,45 +38,41 @@ char *generateword(){
 }
 
 // generates a 2d array of with number of pages
-void* initDictionary(){
+page* initDictionary(){
     pagelock=(pthread_rwlock_t*)malloc(sizeof(pthread_rwlock_t)*pages);
-    dict=(page*)malloc(sizeof(page)*pages);
+
+    page* dict=(page*)malloc(sizeof(page)*pages);
     for(int i=0;i<pages;i++){
         dict[i].words=(void**)malloc(sizeof(void*)*pagelen);
         dict[i].flag=(int*)calloc(pagelen, sizeof(int));
         dict[i].wordcount=0;
-        pthread_rwlock_init(&pagelock[i], NULL);
     }
-    return NULL;
+    return dict;
 }
 
 // adds the word-menaing pair to the dictionary
-int add(void* word){
+int add(page** dict, void* word){
     int pageNumber=tolower(((char*)word)[0])-'a';
-    pthread_rwlock_wrlock(&pagelock[pageNumber]);
-    if(dict[pageNumber].wordcount==pagelen){
-        pthread_rwlock_unlock(&pagelock[pageNumber]);
-        return 0;
-    }
-    pthread_rwlock_wrlock(&countlock);
-    cnt++;
-    pthread_rwlock_unlock(&countlock);
+    // pthread_rwlock_wrlock(&pagelock[pageNumber]);
+    if((*dict)[pageNumber].wordcount==pagelen)
+        return -1;
     int i;
-    dict[pageNumber].wordcount++;
+    (*dict)[pageNumber].wordcount++;
     for(i=0;i<pagelen;i++){
-        if((dict[pageNumber]).flag[i]==0)
+        if(((*dict)[pageNumber]).flag[(i)%pagelen]==0)
             break;
     }
-    dict[pageNumber].words[i]=word;
-    dict[pageNumber].flag[i]=1;
-    pthread_rwlock_unlock(&pagelock[pageNumber]);
+    cnt++;
+    (*dict)[pageNumber].words[i]=word;
+    (*dict)[pageNumber].flag[i]=1;
+    // pthread_rwlock_unlock(&pagelock[pageNumber]);
     return 1;
 }
 
 // returns the meaning of a particular word
 
 // gets the page number and bucket of a particular word in the table
-int find(void* word){
+int find(page* dict, void* word){
     int pageNumber=tolower(((char*)word)[0])-'a';
     pthread_rwlock_wrlock(&pagelock[pageNumber]);
     for(int i=0;i<pagelen;i++){
@@ -95,81 +88,82 @@ int find(void* word){
 }
 
 // // deletes and frees up the particular value from the table
-int delete(void *word){
+int delete(page** dict, void *word){
     int pageNumber=tolower(((char*)word)[0])-'a';
-    int index=find(word);
+    int index=find(*dict, word);
     if(index==-1)
         return 0;
     pthread_rwlock_wrlock(&pagelock[pageNumber]);
-    dict[pageNumber].flag[index]=0;
-    dict[pageNumber].words[index]=NULL;
-    dict[pageNumber].wordcount--;
+    (*dict)[pageNumber].flag[index]=0;
+    (*dict)[pageNumber].words[index]=NULL;
+    (*dict)[pageNumber].wordcount--;
     pthread_rwlock_unlock(&pagelock[pageNumber]);
     return 1;
 }
 
-int deletepage(int pagenumber){
+int deletepage(page** dict, int pagenumber){
     for(int i=0;i<pagelen;i++){
-        if(dict[pagenumber].flag[i]==1){
-            dict[pagenumber].flag[i]=0;
-            dict[pagenumber].words[i]=NULL;
+        if((*dict)[pagenumber].flag[i]==1){
+            (*dict)[pagenumber].flag[i]=0;
+            (*dict)[pagenumber].words[i]=NULL;
         }
-    }
-    dict[pagenumber].wordcount=0;
+    } 
+    (*dict)[pagenumber].wordcount=0;
 }
 
 // prints the entire dictionary
-void *print(){
-    int count=0;
+void print(page* dict){
     for(int i=0;i<pages;i++){
         pthread_rwlock_wrlock(&pagelock[i]);
         for(int j=0;j<pagelen;j++){
             if(dict[i].flag[j]==1){
                 printf("%s\n", (char*)dict[i].words[j]);
-                count++;
             }
         }
         pthread_rwlock_unlock(&pagelock[i]);
         printf("\n");
     }
-    printf("Totals prints : %d", count);
     fflush(stdout);
-    return NULL;
 }
 
 int main(){
-
     struct timeval start, end;
-    pthread_rwlock_init(&countlock, NULL);
-    initDictionary();
+    page* dict=initDictionary();
     char **wordarr=(char**)malloc(sizeof(char*)*datasetlength);
     for(int i=0;i<datasetlength;i++){
         wordarr[i]=generateword();
     } 
-
-    // creating thread
-    pthread_t *addingthreads=(pthread_t*)malloc(sizeof(pthread_t)*datasetlength);
-    // pthread_t addingthreads[datasetlength];
-
+// 2527449
     gettimeofday(&start, NULL);
-
     for(int i=0;i<datasetlength;i++){
-        int k;
-        pthread_create(&addingthreads[i], &k, add, wordarr[i]);
-        // add(wordarr[i]);
-    }
-    for(int i=0;i<datasetlength;i++){
-        pthread_join(addingthreads[i], NULL);
+        add(&dict, wordarr[i]);
     }
     gettimeofday(&end, NULL);
-    printf("Elements added : %d\n",cnt);
+    printf("%d\n", cnt);
     printf("Time taken to add %d elements is : %.ld microseconds\n", datasetlength, (end.tv_sec-start.tv_sec)*1000000L+(end.tv_usec-start.tv_usec));
     fflush(stdout);
-    
     // for(int i=0;i<200;i++){
     //     int index=rand()%datasetlength;
-    //     delete(wordarr[index]);
+    //     delete(&dict, wordarr[index]);
     // }
-    // deletepage(25);
-    // print();
+    // deletepage(&dict, 25);
+    // print(dict);
+    // for(int i=0;i<100;i++){
+    //     int index=rand()%500;
+    //     delete(&dict, wordarr[index]);
+    // }
+    // delete(&dict, wordarr[0]);
+    // sleep(5);
+    // for(int i=0;i<=current_page;i++){
+    //     int t=pagelen[i];
+    //     for(int j=0;j<pagelen[i];j++){
+    //         if(dict[i][j].flag==1)
+    //             printf("%s-%s\n", (char*)dict[i][j].word, (char*)dict[i][j].meaning);
+    //         else
+    //             printf("-:-\n");
+    //         fflush(stdout);
+    //     }
+    //     printf("\n");
+    // }
+    
 }
